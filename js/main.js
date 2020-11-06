8740b6b5c7ec72566f7bccf86555bde9e1782db0
Kliever <64734174+Kliever@users.noreply.github.com>
2020-11-06 16:58:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,30 +10,23 @@ $(function(){
     autoplaySpeed: 5000,
   });
 
-
   new WOW().init();
-
-
-
 
   $(window).on('scroll', function () {
     var cont = $('.content');
     if (!cont.hasClass('values__item-number-1--span')) {
-      $('.values__item-number-1--span').spincrement({    //сюда вставляется класс счетчика, счетчик остается пустым
-        thousandSeparator: '', //разделитель тысяч, можно поставить запятую, тчку и прочее 
-        decimalPoint: '.', //разделитель десятков, можно поставить запятую, тjчку и прочее
-        duration: 4000, //продолжительность анимации в мс
-        from: 0, // не обязательно, с какого числа считать
-        to: 99, // по какое число считать, можно оставить пустым, если вставить в html число
-        decimalPlaces: null, //количество знаков после запятой, если null - определяет по содержимому
-        fade: true // эффект плавного появления
+      $('.values__item-number-1--span').spincrement({
+        thousandSeparator: '',
+        decimalPoint: '.',
+        duration: 4000,
+        from: 0,
+        to: 99,
+        decimalPlaces: null,
+        fade: true
       });
     }
   });
 
-
-  
-// Добавление класса при клике на элемент
   $('.menu-btn').on('click', function () {
     $('.menu-btn').toggleClass('menu-btn--active');
   })
